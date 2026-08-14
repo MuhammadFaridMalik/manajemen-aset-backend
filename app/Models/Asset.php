f3881112d0c58b70d1d65577b70bf635db1ab716
MuhammadFaridMalik <bghvkilik@gmail.com>
2026-08-14 14:22:29 +0700

    fix: tambahkan method generateKodeAset yang hilang

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -7,15 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Asset extends Model
 {
     protected $fillable = [
-        'kode_aset',
-        'nama_aset',
-        'category_id',
-        'location_id',
-        'kondisi',
-        'jumlah',
-        'tanggal_perolehan',
-        'keterangan',
-        'created_by',
+        "kode_aset",
+        "nama_aset",
+        "category_id",
+        "location_id",
+        "kondisi",
+        "jumlah",
+        "tanggal_perolehan",
+        "keterangan",
+        "created_by"
     ];
 
     protected $casts = [
@@ -35,5 +35,14 @@ class Asset extends Model
     public function creator()
     {
         return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public static function generateKodeAset(): string
+    {
+        $lastAsset = self::orderBy('id', 'desc')->first();
+        $lastNumber = $lastAsset ? (int) substr($lastAsset->kode_aset, 4) : 0;
+        $nextNumber = $lastNumber + 1;
+
+        return 'AST-' . str_pad($nextNumber, 4, '0', STR_PAD_LEFT);
     }
 }
