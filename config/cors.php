48620e160ff211cbb0e4535cf06dba1494f89acb
MuhammadFaridMalik <bghvkilik@gmail.com>
2026-08-14 13:42:57 +0700

    chore: update CORS untuk domain netlify

--- config/cors.php
+++ config/cors.php
@@ -8,8 +8,7 @@ return [
     'allowed_origins' => [
         'http://127.0.0.1:5500',
         'http://localhost:5500',
-        // misal:
-        // 'https://aset-sekolah.netlify.app',
+        'https://manajemen-aset-sekolah.netlify.app',
     ],
 
     'allowed_origins_patterns' => [],
